30e4214ef561b503681816449d8d0326ddd51b36
Elia Furlanis <elia.furl@gmail.com>
2021-09-12 20:17:12 +0200

    Added comments for the attributes of the class Collectable

--- Collectable.py
+++ Collectable.py
@@ -1,9 +1,14 @@
 class Collectable:
     def __init__(self, project, id_number, main_folder):
+        # Name of the project the collectable is part of
         self.project = project
+        # Unique number identiying the collectable inside the collection
         self.id = id_number
+        # Main folder of the project 
         self.main_folder = main_folder
+        # Dictionary containing all the attributes and their values
         self.attributes = {}
+        # Export folder for the collectable image
         self.export_folder = ""
 
     def __str__(self):
