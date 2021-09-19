5e3c6329ec755cd85c8cce7742ce004f9cb3a60b
Elia Furlanis <elia.furl@gmail.com>
2021-09-20 00:33:01 +0200

    Implemented skeleton of the collectableGenerator class and test

---
+++ collectableGenerator.py
@@ -0,0 +1,19 @@
+# Implementation of the CollectableGenerator class
+from collectable import Collectable
+
+class CollectableGenerator:
+    def __init__(self, project, main_folder):
+        # List containing all the collectables objects
+        self.collectables = []
+        # String of the project name
+        self.project = project
+        # String of the path to the main folder of the project
+        self.main_folder = main_folder
+
+    def addCollectable(self):
+        id_number = len(self.collectables) + 1
+        new_collectable = Collectable(self.project, id_number, self.main_folder)
+        self.collectables.append(new_collectable)
+
+    def __str__(self):
+        return 'Collectable generator of {} project.\nActual number of collectables = {}\n'.format(self.project, len(self.collectables))
---
+++ testCollectableGenerator.py
@@ -0,0 +1,14 @@
+from collectableGenerator import CollectableGenerator
+
+def main():
+    project = 'Test'
+    main_folder = '.TestProject' 
+    collectableGenerator = CollectableGenerator(project, main_folder)
+
+    collectableGenerator.addCollectable()
+
+    print(collectableGenerator)
+
+if __name__ == '__main__':
+    main()
+    print('DONE!')
