1ed0f829b6e4ac450f0eb9a9ca5fc52b73ffdce6
Elia Furlanis <elia.furl@gmail.com>
2021-09-24 22:05:04 +0200

    Updated collectable class and test changing its attributes

--- Collectable.py
+++ Collectable.py
@@ -1,15 +1,13 @@
 class Collectable:
-    def __init__(self, project, id_number, main_folder):
-        # String containing the name of the project the collectable is part of
+    def __init__(self, project, id_number, attributes_names, attributes_values):
+        # string containing the name of the project the collectable is part of
         self.project = project
-        # Unique number identiying the collectable inside the collection
+        # unique number identiying the collectable inside the collection
         self.id = id_number
-        # String of the path to the main folder of the project 
-        self.main_folder = main_folder
-        # Dictionary containing all the attributes and their values
-        self.attributes = {}
-        # Export folder for the collectable image
-        self.export_folder = ''
+        # tuple containing all the attributes names
+        self.attributes_names = attributes_names
+        # tuple containing all the attributes values
+        self.attributes_values = attributes_values
 
     def __str__(self):
         return 'Collectable #{} of {} project.\n'.format(self.id, self.project)
--- testCollectable.py
+++ testCollectable.py
@@ -3,9 +3,10 @@ from collectable import Collectable
 def main():
     project = 'Test'
     id_number = 3
-    main_folder = 'TestProject'
+    attributes_names = ('BG', 'CORE', 'STAR')
+    attributes_values = ('GREEN', 'BLU', '1')
 
-    new_collectable = Collectable(project, id_number, main_folder)
+    new_collectable = Collectable(project, id_number, attributes_names, attributes_values)
 
     print(new_collectable)
 
