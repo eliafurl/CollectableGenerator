fb0d17420c57884243981ee576f45eb1e93d1052
Elia Furlanis <elia.furl@gmail.com>
2021-10-02 00:17:43 +0200

    Implemented exportCollectable and exportMetadata in class Collectable

--- Collectable.py
+++ Collectable.py
@@ -1,9 +1,14 @@
+from cv2 import imread
+from utilities import *
+
 class Collectable:
     def __init__(self, project, id_number, attributes_names, attributes_values, attributes_imgs):
         # string containing the name of the project the collectable is part of
         self.project = project
         # unique number identiying the collectable inside the collection
         self.id = id_number
+        # collectable name
+        self.name = '{}-{}'.format(self.project, self.id)
         # tuple containing all the attributes names
         self.attributes_names = tuple(attributes_names)
         # tuple containing all the attributes values
@@ -20,3 +25,33 @@ class Collectable:
 
     def __hash__(self):
         return hash((self.attributes_names, self.attributes_values))
+
+    def exportCollectable(self):
+        '''
+        This function returns the stacked image of the collectable, composed of all attributes layers.
+        '''
+        background_path = self.attributes_imgs[0]
+        background = imread(background_path, cv.IMREAD_UNCHANGED)
+        for i in range(1,len(self.attributes_imgs)):
+            foreground_path = self.attributes_imgs[i]
+            foreground = imread(foreground_path, cv.IMREAD_UNCHANGED)
+            background = stackLayers(background, foreground)
+        return background
+
+    def exportMetadata(self, json_export_file):
+        '''
+        This function export the collectable metadata as json file.
+
+        TODO:
+        - create IPFS URI
+        - author
+        '''
+        metadata = { 'name': '{}-{}.png'.format(self.project, self.id),\
+                     'project': self.project,\
+                     'numberID': self.id,\
+                     'hashID': self.identifier,\
+                     'image': 'IPFS URI',\
+                     'attributes': list(self.attributes_values),\
+                     'author': 'me',
+                    }
+        metadataJsonExport(metadata, json_export_file)
--- testCollectable.py
+++ testCollectable.py
@@ -1,16 +1,27 @@
 from collectable import Collectable
+import cv2 as cv
 
 def main():
     project = 'Test'
     id_number = 3
     attributes_names = ('BG', 'CORE', 'STAR')
     attributes_values = ('GREEN', 'BLU', '1')
+    attribute_imgs = ('./TestProject/source_layers/00-BG/BG-GREEN.png',\
+                      './TestProject/source_layers/01-CORE/CORE-BLU.png',\
+                      './TestProject/source_layers/02-STAR/STAR-1.png')
 
-    new_collectable = Collectable(project, id_number, attributes_names, attributes_values, [])
+    new_collectable = Collectable(project, id_number, attributes_names, attributes_values, attribute_imgs)
 
     print(new_collectable)
     print('Hash = {}'.format(hash(new_collectable)))
     assert(new_collectable.identifier == hash(new_collectable))
+
+    stacking_result = new_collectable.exportCollectable()
+    cv.imshow('Test collectable stacking', stacking_result)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
 
 if __name__=='__main__':
     main()
